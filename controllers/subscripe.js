1ac550ad94a7bc0534a0a6a303b4e02acc022d39
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-04-09 16:12:03 +0200

    Fix "returning()" incompatibility with sqlite3

--- controllers/subscripe.js
+++ controllers/subscripe.js
@@ -1,7 +1,8 @@
 module.exports = ({ knex }) => async (req, res) => {
   let feed = (await knex('feeds').select().where('url', req.body.url))[0];
   if (!feed) {
-    feed = await knex('feeds').insert({ url: req.body.url, title: req.body.url }).returning('id');
+    await knex('feeds').insert({ url: req.body.url, title: req.body.url });
+    feed = (await knex('feeds').select().where('url', req.body.url))[0];
   }
 
   await knex('user_feed_subscriptions').insert({ user_id: req.user.id, feed_id: feed.id, pub_date: new Date() });
