f47183efe65311d4336349dcc987972593951933
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-04-09 16:19:52 +0200

    Simplify returning of the id after insert

--- controllers/subscripe.js
+++ controllers/subscripe.js
@@ -1,8 +1,7 @@
 module.exports = ({ knex }) => async (req, res) => {
   let feed = (await knex('feeds').select().where('url', req.body.url))[0];
   if (!feed) {
-    await knex('feeds').insert({ url: req.body.url, title: req.body.url });
-    feed = (await knex('feeds').select().where('url', req.body.url))[0];
+    feed = { id: (await knex('feeds').insert({ url: req.body.url, title: req.body.url }))[0] };
   }
 
   await knex('user_feed_subscriptions').insert({ user_id: req.user.id, feed_id: feed.id, pub_date: new Date() });
