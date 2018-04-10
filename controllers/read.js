abed92779986cd223af6149d74756a2fc3576756
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-04-10 16:35:01 +0200

    Mark single feed items as read

--- controllers/read.js
+++ controllers/read.js
@@ -1,8 +1,8 @@
 module.exports = ({ knex }) => async (req, res) => {
-  await knex('user_feed_item_reads').insert(req.body.feed_item_ids.map(feed_item_id => ({
+  await knex('user_feed_item_reads').insert({
     user_id: req.user.id,
-    feed_item_id: +feed_item_id,
-  })));
+    feed_item_id: +req.body.feed_item_id,
+  });
 
   if (req.accepts('text/html')) {
     res.redirect('/');
