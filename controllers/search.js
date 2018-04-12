53dd09a8e4cb4cf6d9417c0c044e31d1e1a1a5db
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-04-12 16:51:18 +0200

    Catch errors while searching

--- controllers/search.js
+++ controllers/search.js
@@ -2,21 +2,24 @@ const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 
 module.exports = ({ knex }) => async (req, res) => {
-  const html = await fetch(req.query.url).then(res => res.text());
-  const $ = cheerio.load(html);
-
   const links = [];
-  $('link[rel="alternate"][type="application/rss+xml"]').map((_, link) => {
-    link = $(link);
-    const href = link.attr('href');
-    let url;
-    if (href.includes('://')) {
-      url = href;
-    } else {
-      url = req.query.url + href;
-    }
-    links.push({ url, title: link.attr('title') });
-  });
+
+  try {
+    const html = await fetch(req.query.url).then(res => res.text());
+    const $ = cheerio.load(html);
+
+    $('link[rel="alternate"][type="application/rss+xml"]').map((_, link) => {
+      link = $(link);
+      const href = link.attr('href');
+      let url;
+      if (href.includes('://')) {
+        url = href;
+      } else {
+        url = req.query.url + href;
+      }
+      links.push({ url, title: link.attr('title') });
+    });
+  } catch (e) {}
 
   if (req.accepts('text/html')) {
     res.render('search', { url: req.query.url, links });
