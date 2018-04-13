3916c5f2fade00795c3fe6b0ce397f9f38617617
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-04-13 10:53:20 +0200

    Dont redirect if load balancer say it was https and use default status code

--- server.js
+++ server.js
@@ -53,14 +53,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'production') {
   app.set('trust proxy', 1);
   app.use((req, res, next) => {
-    console.log(req.secure);
-    console.log(req.headers);
-    if (req.secure) {
-      console.log('next');
+    if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
       next();
     } else {
-      console.log('redirect', `https://${req.headers['host']}${req.url}`);
-      //res.redirect(301, `https://${req.headers['host']}${req.url}`);
+      res.redirect(`https://${req.headers['host']}${req.url}`);
     }
   });
 }
