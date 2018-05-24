2f8103a84a8ba635a5b1b51b031cf2a86575e0c6
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-05-24 16:13:01 +0200

    Fix undeclared variable

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.engine('handlebars', require('express-handlebars').create({
   defaultLayout: 'main',
   helpers: {
     formatRelative: date => {
-      const momentDate = moment(momentDate);
+      const momentDate = moment(date);
       if (momentDate.isValid()) {
         return moment.duration(momentDate.diff()).humanize(true);
       }
