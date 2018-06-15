fb6200d182096f60dfc81f8d06f8a6b2a6fa0c46
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-06-15 14:02:33 +0200

    Fix SQL error if item.pubDate is undefined

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.use(async (req, res, next) => {
                 categories: item.categories ? item.categories.join(', ') : null,
                 image: item.enclosure && item.enclosure.type.startsWith('image/') ? item.enclosure.url : null,
                 guid: item.guid || item.link,
-                pub_date: new Date(item.pubDate).getTime(),
+                pub_date: item.pubDate ? new Date(item.pubDate).getTime() : new Date().getTime(),
                 author: item['dc:creator'],
               });
               logger.info(`new feed item "${item.title}" for feed "${feed.title} with ID ${feed.id}" and URL ${feed.url}`);
